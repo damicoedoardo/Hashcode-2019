59de57688e8ef5829e4058949eccee213a4da5de
Federico Parroni <keyblade95@live.it>
2019-02-28 20:47:59 +0100

    main and added orientations column

--- dataset/main.py
+++ dataset/main.py
@@ -1,4 +1,11 @@
 from one_hot import one_hot_encoded
+from compute_slideshow import compute
 
-one_hot, tags = one_hot_encoded('dataset/a_example.txt', verbose=True)
+one_hot_matrix, tags, orientations = one_hot_encoded('dataset/a_example.txt', verbose=True)
+
+scores = compute(one_hot_matrix)
+
+orientations = orientations.reshape(-1,1)
+scores = np.hstack((orientations,scores))
+
 
