72c5e2d110999906b3a98f652f5b63fb71a8bfe5
giovanni <giovanni.gabbolini@gmail.com>
2019-03-01 11:44:02 +0100

    min computation

--- compute_slideshow.py
+++ compute_slideshow.py
@@ -2,11 +2,23 @@ import scipy.sparse as sps
 import numpy as np
 from one_hot import one_hot_encoded
 
+def minimum(A, B):
+    BisBigger = A-B
+    BisBigger.data = np.where(BisBigger.data < 0, 0, 1)
+    return A - A.multiply(BisBigger) + B.multiply(BisBigger)
+
 # matrix: a csr matrix containing onehot encodings
-def compute(matrix):
-    return = matrix*matrix.transpose()
+def compute_min(matrix):
+    ones = sps.csr_matrix(np.ones(matrix.shape), dtype=np.int8)
+    matrix_bar = ones - matrix
 
+    fst = matrix*matrix.transpose()
+    snd = matrix_bar*matrix.transpose()
+    trd = matrix*matrix_bar.transpose()
 
-# m,l = one_hot_encoded('dataset/b_lovely_landscapes.txt')
-# compute(m)
-# print(l.toarray())
+    result = minimum(fst, snd)
+    return minimum(result, trd)
+
+m,l,o = one_hot_encoded('dataset/a_example.txt')
+compute(m)
+print(m)
